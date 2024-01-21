2fc39cba888b1235671f4ba860313ea1c8743347
Alexandros Gidarakos <algida79@gmail.com>
2024-01-21 19:11:33 +0000

    feat: add skeleton install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Apply QoL tweaks on a fresh Linux installation.
+#
+# Project URL:
+#   https://github.com/AlexGidarakos/linux-qol-tweaks
+# Author:
+#   Alexandros Gidarakos <algida79@gmail.com>
+#   http://linkedin.com/in/alexandrosgidarakos
+# Standards:
+#   Google Shell Style Guide
+#   https://google.github.io/styleguide/shellguide.html
+# Dependencies:
+#   Bash, GNU coreutils
+# Tested on:
+#   Debian GNU/Linux 12.4 AMD64 LXC container
+# Copyright 2024, Alexandros Gidarakos.
+# SPDX-License-Identifier: MIT
